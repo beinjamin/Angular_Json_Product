8b2be398a6e15088c43c4663b6403d27a586c974
beinjamin <jauresbeinjamin@gmail.com>
2022-05-02 13:57:47 +0100

    Gestion de la route

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { HomeComponent } from './components/home/home.component';
+import { ProductsComponent } from './components/products/products.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {path: "products",
+  component:ProductsComponent
+  },
+  {
+    path: "",
+    component:HomeComponent
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
